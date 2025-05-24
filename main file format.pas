1fe418dfd71e8205cc83abf76256bdc6e39ee9c1
SpaceScripter <joshuadrs@icloud.com.com>
2025-05-24 11:14:33 -0400

    Prep for V2.0.0

---
+++ main file format.pas
@@ -0,0 +1,12 @@
+// This is a simple program to demonstrate the file format.
+// It includes a version number, description, and author information.
+// The program is written in Pascal and uses a simple file format.
+// The file format is as follows:
+    { Program Name: MyProject }
+    { Version: 1.0.0 }
+    { Description: This is a simple program to demonstrate the file format. }
+    { Program Name: MyProject }
+    { Version: 2.0.0 }
+    { Description: Addeed the main filing sytem and stared a better taggsing system }
+    { Author: Joshua Samuel }
+    { Date: 2025-05-24 }
